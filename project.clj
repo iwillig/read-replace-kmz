ff9e2749adda3beaed203e1bcc5aebf65ea0c049
Ivan Willig <iwillig@gmail.com>
2018-02-10 09:11:17 -0500

    Read a kmz, transform it in some way, and write the kmz file back out

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :main read-replace-kmz.core
 
   :profiles {:dev {:source-paths ["dev"]}
-             :uberjar {:aot :all}}
+             :uberjar {:aot :all :uberjar-name "read-replace.jar"}}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
--- src/read_replace_kmz/core.clj
+++ src/read_replace_kmz/core.clj
@@ -7,38 +7,51 @@
   (:import
     (java.io PipedOutputStream)
     (java.io PipedInputStream)
+    (java.io FileOutputStream)
+    (java.io File)
+    (java.nio.file Paths)
     (java.util.zip ZipFile)
     (java.util.zip ZipOutputStream)
     (java.util.zip ZipEntry)))
 
-#_(set! *warn-on-reflection* true)
+(defn write-kmz [^String kmz-file-name name data]
+  (let [out-file (File. kmz-file-name)
+        file-stream (FileOutputStream. out-file)
+        output-stream (ZipOutputStream. file-stream)
+        zip-entry (ZipEntry. ^String name)]
 
-(defn write-kmz [name ]
-  (let []))
+    (.putNextEntry output-stream ^ZipEntry zip-entry)
+    (.write output-stream data 0 (count data))
+    (.closeEntry output-stream)
+    (.close output-stream)))
 
-(defn -main [& args]
-  (let [zip-file (ZipFile. ^String (first args))]
+(defn open-kmz [^File kmz-file]
+  (let [kmz-file-name (.getName kmz-file)
+        parent-path (.getAbsolutePath (.getParentFile kmz-file))
+        new-file-name (str (File. parent-path (str "fix-" kmz-file-name)))
+        zip-file (ZipFile. kmz-file)]
+
     (doseq [file (enumeration-seq (.entries zip-file))]
       (with-open [input-stream (.getInputStream zip-file file)]
-
         (let [original-xml (xml/parse input-stream)
+              ;; TODO, do some transformation of the data
               new-xml (xml/emit-str original-xml)
-              new-stream (io/input-stream (.getBytes new-xml))]
+              new-byte-array (.getBytes new-xml)]
 
-          (pp/pprint [(.getName file)
-                      #_original-xml
-                      new-xml
-                      new-stream
-                      ])
+          (pp/pprint [:transforming (.getAbsolutePath kmz-file) :writing-to new-file-name])
+          (write-kmz new-file-name (.getName file) new-byte-array))))))
 
-          )
+(defn -main [& args]
+  (let [folder (File. ^String (first args))]
 
-        ))))
+    (when (.isDirectory folder)
+      (doseq [file (file-seq folder)]
+        (when-not (.isDirectory file)
+          #_(pp/pprint [:reading (.getName file) :in (.getAbsolutePath file)])
+          (open-kmz file)))))
+
+  )
 
 
 (comment
-
-  (-main "S.kmz")
-
-
-  )
+  (-main "S.kmz"))
