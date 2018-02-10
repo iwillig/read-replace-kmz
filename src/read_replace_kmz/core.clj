d6e8547bdbd5688f2f84e1b7e8c66184e19c9946
Ivan Willig <iwillig@gmail.com>
2018-02-09 23:20:22 -0500

    More things

--- src/read_replace_kmz/core.clj
+++ src/read_replace_kmz/core.clj
@@ -2,25 +2,38 @@
   (:gen-class)
   (:require
     [clojure.pprint :as pp]
-    [clojure.xml :as xml])
+    [clojure.java.io :as io]
+    [clojure.data.xml :as xml])
   (:import
-    (java.util.zip ZipFile)))
+    (java.io PipedOutputStream)
+    (java.io PipedInputStream)
+    (java.util.zip ZipFile)
+    (java.util.zip ZipOutputStream)
+    (java.util.zip ZipEntry)))
+
+#_(set! *warn-on-reflection* true)
+
+(defn write-kmz [name ]
+  (let []))
 
 (defn -main [& args]
   (let [zip-file (ZipFile. ^String (first args))]
-
     (doseq [file (enumeration-seq (.entries zip-file))]
-      (let [xml (.getInputStream zip-file file) ]
+      (with-open [input-stream (.getInputStream zip-file file)]
 
-        (pp/pprint [(.getName file)
-                    #_(slurp xml)
-                    (xml/parse xml)])
+        (let [original-xml (xml/parse input-stream)
+              new-xml (xml/emit-str original-xml)
+              new-stream (io/input-stream (.getBytes new-xml))]
 
-        )
+          (pp/pprint [(.getName file)
+                      #_original-xml
+                      new-xml
+                      new-stream
+                      ])
 
-      )
+          )
 
-    (println "hello there")))
+        ))))
 
 
 (comment
